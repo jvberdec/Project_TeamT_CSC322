9376f3f3e02ded12b352452d8ea571298d8892cc
jvberdec <44758976+jvberdec@users.noreply.github.com>
2021-10-28 19:27:54 -0400

    added base/index files
    added base/index: the foundation for our site

--- flask/Pages.py
+++ flask/Pages.py
@@ -1,55 +1,68 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Home Page"
+    # return "Home Page"
+    return render_template("index.html")
+
 
 @app.route("/student_app")
 def student_app():
     return "Student Application"
 
+
 @app.route("/instructor_app")
 def instructor_app():
     return "Instructor Application"
+
 
 @app.route("/student_dash")
 def student_dash():
     return "Student Dashboard"
 
+
 @app.route("/instructor_dash")
 def instructor_dash():
     return "Instructor Dashboard"
 
+
 @app.route("/registrar_default_dash")
-def registrar_dash():
+def registrar_default_dash():
     return "Registrar Default Dashboard"
 
-@app.route("/registrar_class_set_up")
-def registrar_dash():
+
+@app.route("/registrar_class_setup")
+def registrar_class_setup():
     return "Registrar Class Set Up"
 
+
 @app.route("/registrar_course_reg")
-def registrar_dash():
+def registrar_course_reg():
     return "Registrar Course Registration"
 
-@app.route("/registrar_class run_period")
-def registrar_dash():
+
+@app.route("/registrar_class_run_period")
+def registrar_class_run_period():
     return "Registrar Class Running Period"
 
+
 @app.route("/registrar_grading_period")
-def registrar_dash():
+def registrar_grading_period():
     return "Registrar Grading Period"
+
 
 @app.route("/<name>")
 def user(name):
     return f"Hello {name}!"
 
+
 @app.route("/admin")
 def admin():
     return redirect(url_for("home"))
 
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
