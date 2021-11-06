4e9a4b1775247a8fa6b16caf6dab7e7de44d64e0
baishakicodes <bdebi5985@bths.edu>
2021-11-06 17:45:36 -0400

    created sign in page

--- flask/Pages.py
+++ flask/Pages.py
@@ -1,8 +1,12 @@
-from flask import Flask, redirect, url_for, render_template, request, session
-from forms import ApplicationForm, StudentAppplicationForm, LoginForm
+import os
+from dotenv import load_dotenv
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+from forms import ApplicationForm, StudentApplicationForm, LoginForm
 from datetime import timedelta
-
+load_dotenv()
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
 
 
 @app.route("/")
@@ -11,15 +15,25 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/login/", methods=['POST', 'GET'])
+@app.route("/login", methods=['POST', 'GET'])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.user_type.data == 'student':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('student_dash'))
+        elif login_form.user_type.data == 'instructor':
+            return redirect(url_for('instructor_dash'))
+        elif login_form.user_type.data == 'registrar':
+            return redirect(url_for('registrar_default_dash'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=login_form)
 
 
 @app.route("/student_app")
 def student_app():
-    student_app_form = StudentAppplicationForm()
+    student_app_form = StudentApplicationForm()
     return render_template('student_app.html', title='Student Application', form=student_app_form)
 
 
