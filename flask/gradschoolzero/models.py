d37475c06d1cf1e77b66131288ea7df94e2258e8
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2021-11-16 01:20:17 -0500

    organize models and add inheritance

--- flask/gradschoolzero/models.py
+++ flask/gradschoolzero/models.py
@@ -1,72 +1,37 @@
 from datetime import datetime, date
-from gradschoolzero import db
+from gradschoolzero import db, login_manager
+from flask_login import UserMixin
 
 
-class User(db.Model):
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    logged_in_before = db.Column(db.Boolean, nullable=False, default=False)
-    role = db.Column(db.String(10), nullable=False)
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class StudentApplicant(db.Model):
-    __tablename__ = 'student_applicant'
+class Applicant(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     dob = db.Column(db.Date, nullable=False)
+
+
+class StudentApplicant(Applicant):
+    __tablename__ = 'student_applicant'
+    id = db.Column(db.Integer, db.ForeignKey('applicant.id'), primary_key=True)
     gpa = db.Column(db.Float, nullable=False)
 
     def __repr__(self):
         return f"StudentApplicant('{self.first_name}', '{self.last_name}', '{self.email}', '{self.dob}', '{self.gpa}')"
 
 
-class InstructorApplicant(db.Model):
+class InstructorApplicant(Applicant):
     __tablename__ = 'instructor_applicant'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(120), nullable=False)
-    dob = db.Column(db.Date, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey('applicant.id'), primary_key=True)
     discipline = db.Column(db.String(20), nullable=False)
 
 
-class StudentCourseEnrollment(db.Model):
-    __tablename__ = 'student_course_enrollment'
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
-    course_section_id = db.Column(db.Integer, db.ForeignKey('course_section.id'), nullable=False)
-    grade = db.Column(db.String(1))
-
-    students = db.relationship('Student', back_populates='courses_enrolled')
-    courses = db.relationship('CourseSection', back_populates='students_enrolled')
-
-    def __repr__(self):
-        return f"StudentCourseEnrollment('{self.id}', '{self.student_id}', '{self.course_section_id}', '{self.grade}')"
-    
-
-class Student(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    gpa = db.Column(db.Float, nullable=False)
-    special_registration = db.Column(db.Boolean, nullable=False)
-    status = db.Column(db.String(20), nullable=False)
-
-    user = db.relationship("User", backref='user', uselist=False)
-    courses_enrolled = db.relationship('StudentCourseEnrollment', back_populates='students')
-    complaints_filed = db.relationship('StudentComplaint', back_populates='students')
-    warnings = db.relationship('StudentWarning', backref='student')
-
-
-    def __repr__(self):
-        return f"Student('{self.username}', '{self.email}', '{self.first_name}', '{self.last_name}', '{self.gpa}')"
-
-
-class Instructor(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), nullable=False)
@@ -74,6 +39,26 @@ class Instructor(db.Model):
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
     logged_in_before = db.Column(db.Boolean, nullable=False, default=False)
+    type = db.Column(db.String(10), nullable=False)
+    
+
+class Student(User):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    gpa = db.Column(db.Float, nullable=False)
+    special_registration = db.Column(db.Boolean, nullable=False)
+    status = db.Column(db.String(20), nullable=False)
+
+    courses_enrolled = db.relationship('StudentCourseEnrollment', back_populates='students')
+    complaints_filed = db.relationship('StudentComplaint', back_populates='students')
+    warnings = db.relationship('StudentWarning', backref='student')
+
+
+    def __repr__(self):
+        return f"Student('{self.username}','{self.gpa}')"
+
+
+class Instructor(User):
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     status = db.Column(db.String, nullable=False)
 
     sections = db.relationship('CourseSection', back_populates='instructor')
@@ -103,8 +88,22 @@ class CourseSection(db.Model):
     courses = db.relationship('Course', backref='section')
     students_enrolled = db.relationship('StudentCourseEnrollment', back_populates='courses')
 
+
+class StudentCourseEnrollment(db.Model):
+    __tablename__ = 'student_course_enrollment'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    course_section_id = db.Column(db.Integer, db.ForeignKey('course_section.id'), nullable=False)
+    grade = db.Column(db.String(1))
+
+    students = db.relationship('Student', back_populates='courses_enrolled')
+    courses = db.relationship('CourseSection', back_populates='students_enrolled')
+
+    def __repr__(self):
+        return f"StudentCourseEnrollment('{self.id}', '{self.student_id}', '{self.course_section_id}', '{self.grade}')"
+
     
-class CensorWords(db.Model):
+class CensorWord(db.Model):
     __tablename__ = 'censor_word'
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(20), nullable=False)
@@ -143,18 +142,20 @@ class InstructorComplaint(db.Model):
     instructors = db.relationship('Instructor', back_populates='complaints_filed')
 
 
-class StudentWarning(db.Model):
-    __tablename__ = 'student_warning'
+class Warning(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     date_received = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     warning = db.Column(db.Text, nullable=False)
+
+
+class StudentWarning(Warning):
+    __tablename__ = 'student_warning'
+    id = db.Column(db.Integer, db.ForeignKey('warning.id'), primary_key=True)
     student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
 
 
-class InstructorWarning(db.Model):
+class InstructorWarning(Warning):
     __tablename__ = 'instructor_warning'
-    id = db.Column(db.Integer, primary_key=True)
-    date_received = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    warning = db.Column(db.Text, nullable=False)
+    id = db.Column(db.Integer, db.ForeignKey('warning.id'), primary_key=True)
     instructor_id = db.Column(db.Integer, db.ForeignKey('instructor.id'), nullable=False)
 
