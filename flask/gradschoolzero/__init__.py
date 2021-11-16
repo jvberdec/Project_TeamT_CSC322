2fb190d28b51c8261e0cc086ef66070a8e0e058d
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2021-11-16 01:11:51 -0500

    add login_manager and email login info

--- flask/gradschoolzero/__init__.py
+++ flask/gradschoolzero/__init__.py
@@ -3,12 +3,18 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['EMAIL_ADDRESS'] = os.environ.get('EMAIL_ADDR')
+app.config['EMAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from gradschoolzero import routes
