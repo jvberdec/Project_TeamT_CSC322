b5752fdf45a7d6ef6983a78e03609e28b08fe875
Daniel Aguilar-Rodriguez <daniel.a-r@outlook.com>
2021-12-01 01:54:54 -0500

    Add registrar user to db

--- flask/run.py
+++ flask/run.py
@@ -1,5 +1,17 @@
-from gradschoolzero import app, db
+from gradschoolzero import app, db, bcrypt
+from gradschoolzero.models import User
 
 if __name__ == "__main__":
     db.create_all()
+    registrar = User.query.filter_by(username='admin', type='registrar').first()
+    if registrar is None:
+        hashed_password = bcrypt.generate_password_hash(app.config['MAIL_PASSWORD']).decode('utf-8')
+        registrar = User(username='admin',
+                         email=app.config['MAIL_USERNAME'],
+                         password=hashed_password,
+                         first_name='',
+                         last_name='',
+                         type='registrar')
+        db.session.add(registrar)
+        db.session.commit()
     app.run(debug=True)
